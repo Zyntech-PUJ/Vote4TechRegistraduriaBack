378814043f0dccc478fa6e4fd1f2c7cafc6e79a5
Erick Salazar Suarez <ericksalazar@javeriana.edu.co>
2026-04-17 16:31:34 -0500

    commit inicial

---
+++ src/main/java/PortalRegistraduriaBack/PortalRegistraduriaBackApplication.java
@@ -0,0 +1,13 @@
+package PortalRegistraduriaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRegistraduriaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalRegistraduriaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PortalRegistraduriaBack/PortalRegistraduriaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package PortalRegistraduriaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalRegistraduriaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
